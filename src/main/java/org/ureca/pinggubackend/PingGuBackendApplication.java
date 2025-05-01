54cf57fbb695557b254d15ded9d995b3e62a5e80
k0000k <locker50@naver.com>
2025-05-01 15:14:42 +0900

    add: init

---
+++ src/main/java/org/ureca/pinggubackend/PingGuBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ureca.pinggubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingGuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingGuBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ureca/pinggubackend/PingGuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ureca.pinggubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingGuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
