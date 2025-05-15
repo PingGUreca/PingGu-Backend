482b39b2689224ffc152eefbc07801f17da4c78f
k0000k <locker50@naver.com>
2025-05-15 21:30:37 +0900

    [fix] 토큰 저장을 위한 설정

--- src/main/java/org/ureca/pinggubackend/global/config/SecurityConfig.java
+++ src/main/java/org/ureca/pinggubackend/global/config/SecurityConfig.java
@@ -61,6 +61,7 @@ public class SecurityConfig {
         );
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("Authorization"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
 
